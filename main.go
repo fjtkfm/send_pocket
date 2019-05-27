794d731e78b3bbea358dd9b4380e0f0346c2194a
piruty <8793641+piruty@users.noreply.github.com>
2019-05-27 12:34:33 +0900

    りふぁくたりんぐ

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"bytes"
 	"encoding/json"
+	"errors"
 	"flag"
 	"fmt"
 	"io/ioutil"
@@ -45,6 +46,24 @@ type FetchItem struct {
 	Tags                   map[string]map[string]string `json:"tags"`
 }
 
+func (fetchItem FetchItem) Title() string {
+	if len(fetchItem.ResolvedTitle) > 0 {
+		return fetchItem.ResolvedTitle
+	}
+	return fetchItem.GivenTitle
+}
+
+func (fetchItem FetchItem) Url() string {
+	if len(fetchItem.ResolvedUrl) > 0 {
+		return fetchItem.ResolvedUrl
+	}
+	return fetchItem.GivenUrl
+}
+
+func (fetchItem FetchItem) String() string {
+	return fmt.Sprintf("%s ( %s )\n", fetchItem.Title(), fetchItem.Url())
+}
+
 func main() {
 	consumerKey := os.Getenv("POCKET_CONSUMER_KEY")
 	accessToken := os.Getenv("POCKET_ACCESS_TOKEN")
@@ -52,99 +71,111 @@ func main() {
 
 	isArchive := flag.Bool("a", false, "archive flag: default false")
 	number := flag.Int("n", 10, "item count")
-	sendSlack := flag.Bool("s", false, "send slack flag: default false")
+	isSent := flag.Bool("s", false, "send slack flag: default false")
 	slackUrl := flag.String("url", defaultSlackUrl, "slack channel url: env value of SLACK_POCKET_URL")
 
 	flag.Parse()
 
-	params := url.Values{}
-	params.Set("state", "unread")
-	params.Set("sort", "newest")
-	params.Set("count", strconv.Itoa(*number))
-	params.Set("consumer_key", consumerKey)
-	params.Set("access_token", accessToken)
-
-	resp, err := http.Get("https://getpocket.com/v3/get?" + params.Encode())
-	if err != nil {
-		fmt.Println(err.Error())
-		return
-	}
-	defer resp.Body.Close()
-
-	body, err := ioutil.ReadAll(resp.Body)
-	if err != nil {
-		fmt.Println(err.Error())
-		return
-	}
-
-	if resp.StatusCode != http.StatusOK {
-		fmt.Println(string(body))
-		return
-	}
-
-	var result FetchResult
-	err = json.Unmarshal(body, &result)
+	items, err := getPocketItems(*number, consumerKey, accessToken)
 	if err != nil {
 		fmt.Println(err.Error())
 		return
 	}
 
 	text := ""
-	for _, fetchItem := range result.List {
-		var title string
-		if len(fetchItem.ResolvedTitle) > 0 {
-			title = fetchItem.ResolvedTitle
-		} else {
-			title = fetchItem.GivenTitle
-		}
-		var itemUrl string
-		if len(fetchItem.ResolvedUrl) > 0 {
-			itemUrl = fetchItem.ResolvedUrl
-		} else {
-			itemUrl = fetchItem.GivenUrl
-		}
-		text += fmt.Sprintf("%s ( %s )\n", title, itemUrl)
+	for _, fetchItem := range items {
+		text += fmt.Sprint(fetchItem)
 
 		if *isArchive {
-			actions := fmt.Sprintf("[{\"action\":\"archive\",\"item_id\":\"%s\",\"time\":\"%d\"}]", fetchItem.ItemId, time.Now().Unix())
-
-			params := url.Values{}
-			params.Set("actions", actions)
-			params.Set("consumer_key", consumerKey)
-			params.Set("access_token", accessToken)
-
-			resp, err := http.Get("https://getpocket.com/v3/send?" + params.Encode())
+			result, err := archiveItem(fetchItem, consumerKey, accessToken)
 			if err != nil {
 				fmt.Println(err.Error())
 				continue
 			}
-
-			body, err := ioutil.ReadAll(resp.Body)
-			if err != nil {
-				fmt.Println(err.Error())
-				continue
-			}
-
-			if resp.StatusCode != http.StatusOK {
-				fmt.Println(string(body))
-				continue
-			}
-
-			fmt.Printf(`Title: "%s" is arhived\n`, title)
+			fmt.Println(result)
 		}
 	}
 
-	if !(*sendSlack) {
+	if !(*isSent) {
 		fmt.Println(text)
 		return
 	}
 
-	req, err := http.NewRequest(
-		"POST",
-		*slackUrl,
-		bytes.NewBuffer([]byte(`{"text":"`+text+`"}`)))
+	result, err := sendSlack(*slackUrl, text)
 	if err != nil {
 		fmt.Println(err.Error())
+		return
+	}
+
+	fmt.Println(result)
+}
+
+func getPocketItems(number int, consumerKey, accessToken string) (list map[int]FetchItem, err error) {
+	params := url.Values{}
+	params.Set("state", "unread")
+	params.Set("sort", "newest")
+	params.Set("count", strconv.Itoa(number))
+	params.Set("consumer_key", consumerKey)
+	params.Set("access_token", accessToken)
+
+	resp, err := http.Get("https://getpocket.com/v3/get?" + params.Encode())
+	if err != nil {
+		return
+	}
+	defer resp.Body.Close()
+
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return
+	}
+
+	if resp.StatusCode != http.StatusOK {
+		err = errors.New(string(body))
+		return
+	}
+
+	var result FetchResult
+	err = json.Unmarshal(body, &result)
+	if err != nil {
+		return
+	}
+
+	return result.List, nil
+}
+
+func archiveItem(fetchItem FetchItem, consumerKey, accessToken string) (result string, err error) {
+	actions := fmt.Sprintf("[{\"action\":\"archive\",\"item_id\":\"%s\",\"time\":\"%d\"}]", fetchItem.ItemId, time.Now().Unix())
+
+	params := url.Values{}
+	params.Set("actions", actions)
+	params.Set("consumer_key", consumerKey)
+	params.Set("access_token", accessToken)
+
+	resp, err := http.Get("https://getpocket.com/v3/send?" + params.Encode())
+	if err != nil {
+		return
+	}
+
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return
+	}
+
+	if resp.StatusCode != http.StatusOK {
+		err = errors.New(string(body))
+		return
+	}
+
+	result = fmt.Sprintf(`Title: "%s" is arhived\n`, fetchItem.Title())
+	return
+}
+
+func sendSlack(slackUrl, text string) (result string, err error) {
+	req, err := http.NewRequest(
+		"POST",
+		slackUrl,
+		bytes.NewBuffer([]byte(`{"text":"`+text+`"}`)))
+	if err != nil {
 		return
 	}
 
@@ -153,10 +184,15 @@ func main() {
 	client := &http.Client{}
 	res, err := client.Do(req)
 	if err != nil {
-		fmt.Println(err.Error())
 		return
 	}
 	defer res.Body.Close()
 
-	fmt.Println(res)
+	body, err := ioutil.ReadAll(res.Body)
+	if err != nil {
+		return
+	}
+
+	result = string(body)
+	return
 }
